d4737f9b05afaada42a62bbda55e4328ec08ae35
Melvin Tang <mxcodeyard@gmail.com>
2020-10-30 17:20:09 +0800

    Add mongodb & create db model

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+import { ApolloServer } from 'apollo-server-express';
+import { resolvers } from './resolvers/index.js';
+import { typeDefs } from './typeDefs/index.js';
+import mongoose from 'mongoose';
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
 
-const typeDefs = gql`
-  type Query {
-    hello: String
+(async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/graphql', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+
+    const app = express();
+    server.applyMiddleware({ app });
+
+    app.listen({ port: 3000 }, () =>
+      console.log('Now browse to http://localhost:3000' + server.graphqlPath)
+    );
+  } catch (error) {
+    console.error(error);
   }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello World',
-  },
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-const app = express();
-server.applyMiddleware({ app });
-
-app.listen({ port: 3000 }, () =>
-  console.log('Now browse to http://localhost:3000' + server.graphqlPath)
-);
+})();
---
+++ models/Product.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+export default mongoose.model('Product', productSchema);
